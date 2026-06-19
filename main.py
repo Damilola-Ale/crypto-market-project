9b9929644f1bdf5cdb78b838c1fa4af9cfe9e331
Damilola-Ale <ale.destined@gmail.com>
2026-06-19 10:03:42 +0100

    update

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ def fetch_binance(symbol, interval, limit):
 #     "IDUSDT", "VICUSDT", "ETHUSDT", "ZECUSDT", "DEXEUSDT", "RPLUSDT", "GRTUSDT",
 #     "RENDERUSDT", "PAXGUSDT", "IOSTUSDT", "KNCUSDT", "KAVAUSDT", "RLCUSDT", "BELUSDT"
 # ] IOST KNC KAVA RLC | KSM BEL
-SYMBOL = "AAVEUSDT"
+SYMBOL = "ICXUSDT"
 
 LLTF_INTERVAL = "5m"
 LTF_INTERVAL = "1h"
@@ -144,9 +144,9 @@ HTF_LIMIT = 4380   # ~120 days of 4h candles
 # LTF_LIMIT = 1000   # ~30 days of 1h candles
 # HTF_LIMIT = 250   # ~120 days of 4h candles
 
-LLTF_LIMIT = 9600
-LTF_LIMIT = 800   # ~30 days of 1h candles
-HTF_LIMIT = 200   # ~120 days of 4h candles
+# LLTF_LIMIT = 9600
+# LTF_LIMIT = 800   # ~30 days of 1h candles
+# HTF_LIMIT = 200   # ~120 days of 4h candles
 
 # ==========================================================
 # FETCH DATA
